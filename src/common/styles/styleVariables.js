e6de1e3a7322dcd92aee78ae1de1d59ae25c3981
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-08-19 00:16:13 +0900

    uploading styleVariables.js

---
+++ src/common/styles/styleVariables.js
@@ -0,0 +1,33 @@
+// whites
+export const white100 = `rgba(255, 255, 255, 1)` // #FFF
+export const white70 = `rgba(255, 255, 255, 0.7)`;
+export const white50 = `rgba(255, 255, 255, 0.5)`;
+export const white20 = `rgba(255, 255, 255, 0.2)`;
+
+// grays
+export const gray343434 = `rgba(52, 52, 52, 1)` // #343434
+export const gray3C3C3C = `rgba(60, 60, 60, 1)` // #3c3c3c
+export const gray616161 = `rgba(97, 97, 97, 1)` // #616161
+export const gray757575 = `rgba(117, 117, 117, 1)` // #757575
+export const gray858585 = `rgba(133, 133, 133, 1)` // #858585
+export const gray8B95A1 = `rgba(139, 149, 161, 1)` // #8B95A1
+export const grayA9A9A9 = `rgba(169, 169, 169, 1)` // #a9a9a9
+export const grayAFAFAF = `rgba(175, 175, 175, 1)` // #AFAFAF
+export const grayB9B9B9 = `rgba(185, 185, 185, 1)` // #B9b9b9
+export const grayD9D9D9 = `rgba(217, 217, 217, 1)` // #D9D9D9 // commentGray 및 commentGray50을 대체
+export const grayD9D9D950 = `rgba(217, 217, 217, 0.5)`
+export const grayE9E9E9 = `rgba(233, 233, 233, 1)` // #E9E9E9
+export const grayF2F2F2 = `rgba(242, 242, 242, 1)` // #F2F2F2
+
+// reds
+export const redFF8989 = `rgba(255, 137, 137, 1)`; //#FF8989
+
+// blues
+export const blue004B8F = `rgba(0, 75, 143, 1)` //#004b8f
+export const blue7EC6CC = `rgba(126, 198, 204, 1)` //#7EC6CC
+
+// yellows -> 수정사항 생길지도 몰라...
+export const yellowEE9542 = `rgba(238, 149, 66, 1)` //#EE9542
+export const yellowFFB571 = `rgba(255, 181, 113, 1)` // #FFB571
+export const yellowFFBE82 = `rgba(255, 190, 130, 1)` //#FFBE82
+export const yellowFFFFF3 = `rgba(255, 255, 243, 1)` //#FFFFF3
