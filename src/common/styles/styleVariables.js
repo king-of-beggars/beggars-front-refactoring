6bec185a5b68d7d5b64dffe3b0251195eac7de79
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-08-19 20:51:50 +0900

    변수 등록 완료

--- src/common/styles/styleVariables.js
+++ src/common/styles/styleVariables.js
@@ -8,10 +8,12 @@ export const white20 = `rgba(255, 255, 255, 0.2)`; //
 export const gray343434 = `rgba(52, 52, 52, 1)` // #343434 //
 export const gray3C3C3C = `rgba(60, 60, 60, 1)` // #3c3c3c //
 export const gray484848 = `rgba(72, 72, 72, 1)` // #484848 //
+export const gray595959 = `rgba(89, 89, 89, 1)` // #595959 // modal btn border
 export const gray6C6C6C = `rgba(108, 108, 108, 1)` // #6c6c6c //
 export const gray757575 = `rgba(117, 117, 117, 1)` // #757575 //
 export const gray858585 = `rgba(133, 133, 133, 1)` // #858585 //
 export const gray8B95A1 = `rgba(139, 149, 161, 1)` // #8B95A1 //
+export const gray909090 = `rgba(144, 144, 144, 1)` // #909090 // profile circle border
 export const gray9C9C9C = `rgba(156, 156, 156, 1)` // #9c9c9c //
 export const grayA9A9A9 = `rgba(169, 169, 169, 1)` // #a9a9a9 //
 export const grayAFAFAF = `rgba(175, 175, 175, 1)` // #AFAFAF // 
@@ -23,6 +25,8 @@ export const grayE9E9E9 = `rgba(233, 233, 233, 1)` // #E9E9E9 //
 
 // reds
 export const redFF8989 = `rgba(255, 137, 137, 1)`; //#FF8989 //
+export const redFF4A55 = `rgba(255, 74, 85, 1)` // #FF4A55 // progress bar
+export const redEB3E3E = `rgba(235, 62, 62, 1)` // #EB3E3E // likes
 
 // blues
 export const blue004B8F = `rgba(0, 75, 143, 1)` //#004b8f //
