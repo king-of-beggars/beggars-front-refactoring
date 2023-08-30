061ba504826832f9c2207ff4c691cf462ae311e8
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-08-30 22:16:40 +0900

    Webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  resolve: {
+    alias: {
+        "@common-assets": path.resolve(__dirname, "src/common/assets/"),
+        "@common-components": path.resolve(__dirname, "src/common/components/"),
+        "@common-constants": path.resolve(__dirname, "src/common/constants/"),
+        "@common-providers": path.resolve(__dirname, "src/common/providers/"),
+        "@common-styles": path.resolve(__dirname, "src/common/styles/"),
+        "@common-utils": path.resolve(__dirname, "src/common/utils/"),
+        "@feature-auth" : path.resolve(__dirname, "src/features/auth"),
+        "@feature-board" : path.resolve(__dirname, "src/features/board"),
+        "@feature-cashbook" : path.resolve(__dirname, "src/features/cashbook"),
+        "@feature-deal" : path.resolve(__dirname, "src/features/deal"),
+        "@feature-main" : path.resolve(__dirname, "src/features/main"),
+        "@router" : path.resolve(__dirname, "src/router")
+    }
+  }
+};
