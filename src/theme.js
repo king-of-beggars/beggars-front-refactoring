1230ed032b310e5a6ee52ee674617a72b00f2789
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-09-10 22:10:26 +0900

    Styled-component 설치, ThemeProvider 적용

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+const theme = {
+    color: {
+        blacks: {
+            100: "#9C9C9C",
+            200: "#6C6C6C",
+            300: "#3C3C3C",
+            400: "#000000"
+        }
+    },
+    modal: {
+        padding: {
+            sm: "16px",
+            md: "24px",
+            lg: "32px"
+        },
+        gap: {
+            sm: "14px",
+            md: "20px",
+            lg: "34px"
+        }
+    },
+    button: {
+        width: {
+            xl: "330px",
+            lg: "292px",
+            sm: "150px",
+            xs: "100px"
+        },
+        height: "50px",
+        gap: {
+            modal: "10px"
+        }
+    }
+}
+
+
+export default theme
