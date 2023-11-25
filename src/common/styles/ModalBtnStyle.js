ea270b9bb8a8c0e259d268e9b36f66dbe9cd650d
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-11-25 20:27:05 +0900

    [style] ModalBtnStyle.js 기본 폰트 사이즈 변경

--- src/common/styles/ModalBtnStyle.js
+++ src/common/styles/ModalBtnStyle.js
@@ -5,7 +5,7 @@ const ModalBtnStyle = styled.button`
     height: ${props => props.theme.button.height};
     background-color: ${props => !!props.color ? props.color : props.theme.color.grayscale[900]};
     font-family: ${props => props.theme.font.serif};
-    font-size: ${props => props.theme.font.size.default};
+    font-size: ${props => props.theme.font.size.sm};
     color: ${props => props.theme.color.grayscale[0]};
     
     border: none;
